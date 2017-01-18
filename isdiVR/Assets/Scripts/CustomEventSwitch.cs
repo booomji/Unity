88c724fd6341772dae2d7166ab90a74ac45ab960
behram <batwrk@gmail.com>
2017-01-18 14:18:01 +0530

    ISDIVR
    class on Events and UI

---
+++ isdiVR/Assets/Scripts/CustomEventSwitch.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomEventSwitch : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+
+
+	public void SwitchFunc(int num)
+	{
+		switch(num)
+		{
+		case 1:
+			print ("intput X");
+			break;
+
+		case 2:
+			print ("input Y");
+			break;
+
+		default:
+			print ("invalid arg");
+			break;
+		
+		}
+
+	}
+
+	public void TestFunc(int num)
+	{
+		Debug.Log (num.ToString() + "test");
+//		print (num.ToString() + "test");
+	}
+}
---
+++ isdiVR/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour {
+
+
+	public GameObject prefab;
+	public RectTransform barTransform;
+	float damage = 100f;
+	float increment = 10;
+	public AudioSource playSound;
+
+	// Use this for initialization
+	void Start () {
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Health()
+	{
+		
+		float heal = 50f;
+
+		if (barTransform.sizeDelta.x > 0) {
+			barTransform.sizeDelta = new Vector2 (damage-=20f,barTransform.sizeDelta.y); 
+			playSound.Play ();
+			
+		}
+
+			
+					
+
+//		Debug.Log("health damage ");
+
+	}
+
+	public void Instantiate()
+	{
+		
+		if (barTransform.sizeDelta.x > 0) {
+			
+			Button buttonObj = GetComponentInChildren<Button>();
+			Vector3 posAdd = buttonObj.transform.position + ( Vector3.forward * increment );
+
+			GameObject obj = (GameObject)Instantiate( prefab,posAdd,barTransform.rotation );
+			increment += 10;
+			Debug.Log ("value if increment is" + increment.ToString());
+			
+		}
+
+	}
+}
